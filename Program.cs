ca5633ca56b48f96cbbeddbccab64dd50ac123bc
carolineconley <carolineconley@CAROLINECONB4F4>
2024-01-17 07:51:17 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+// caroline conley
+class DiceSimulator
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.Write("How many dice rolls would you like to simulate? ");
+        int numberOfRolls = int.Parse(Console.ReadLine());
+
+        DiceRoller roller = new DiceRoller();
+        int[] results = roller.SimulateRolls(numberOfRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numberOfRolls}.\n");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = results[i] * 100 / numberOfRolls;
+            Console.WriteLine($"{i}: {new string('*', percentage)}");
+        }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
+
+class DiceRoller
+{
+    private Random random = new Random();
+
+    public int[] SimulateRolls(int numberOfRolls)
+    {
+        int[] results = new int[13]; // Index 0 is not used, 2 through 12 represent dice combinations
+
+        for (int i = 0; i < numberOfRolls; i++)
+        {
+            int dice1 = random.Next(1, 7);
+            int dice2 = random.Next(1, 7);
+            int sum = dice1 + dice2;
+            results[sum]++;
+        }
+
+        return results;
+    }
+}
